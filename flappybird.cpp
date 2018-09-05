bd2ac84bd29145c55e56c89187738cb37eb0c367
Hernan Quintana <hernan@jsatch.com>
2018-09-04 20:03:06 -0500

    Improving game cycle

--- flappybird.cpp
+++ flappybird.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2_image/SDL_image.h>
+#define FPS 60
 
 using namespace std;
 
@@ -31,6 +32,41 @@ int main(int argc, char const *argv[])
         cout << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
         SDL_Quit();
         return 1;
+    }
+
+    SDL_Surface *screen = SDL_GetWindowSurface(win);
+    if (screen == nullptr)
+    {
+        SDL_FreeSurface(screen);
+        SDL_DestroyRenderer(ren);
+        SDL_DestroyWindow(win);
+        cout << "SDL_CreateRenderer Error" << SDL_GetError() << endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+    Uint32 starting_tick;
+    while(running)
+    {
+        starting_tick = SDL_GetTicks();
+        while (SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_QUIT:
+                    SDL_FreeSurface(screen);
+                    SDL_DestroyRenderer(ren);
+                    SDL_DestroyWindow(win);
+                    SDL_Quit();
+                break;
+            }
+        }
+        if (1000 / FPS > SDL_GetTicks() - starting_tick)
+        {
+            SDL_Delay((1000 / FPS) - SDL_GetTicks() - starting_tick);
+        }
     }
 
     
